ca073fa0a94ad0ae086c854953bb8427c5a28403
Demian <dnetlyukh@gmail.com>
2022-09-22 17:46:24 +0100

    Add migration assets

---
+++ server/src/db/index.js
@@ -0,0 +1,53 @@
+import fs from 'fs'
+import path from 'path'
+import { Sequelize } from 'sequelize'
+import configs from './config/config.json'
+
+const basename = path.basename(__filename)
+
+const env = process.env.NODE_ENV ?? 'development'
+if (env !== 'development') {
+  throw new Error('Invalid environment value')
+}
+
+const config = configs[env]
+if (config.dialect !== 'mysql' && config.dialect !== 'postgres' &&
+  config.dialect !== 'sqlite' && config.dialect !== 'mariadb' &&
+  config.dialect !== 'mssql' && config.dialect !== 'db2' &&
+  config.dialect !== 'snowflake') {
+  throw new Error('Invalid dialect value')
+}
+
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+  ...config,
+  dialect: config.dialect
+})
+
+// FIXME: Add typing
+const db = {
+  sequelize,
+  Sequelize
+}
+
+// export const models: IModels = {}
+const modelsPath = path.resolve('..', 'api', 'models')
+fs
+  .readdirSync(modelsPath)
+  .filter((file) => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js')
+  })
+  .forEach(async (file) => {
+    // FIXME: Add typing
+    const model = await import(file)
+
+    // const model = require(path.join(__dirname, file))(sequelize, DataTypes)
+    db[model.name] = model
+  })
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db)
+  }
+})
+
+export default db
