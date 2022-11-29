53c3574e97f52abaca3bcf13fe71811274bebdad
Demian <dnetlyukh@gmail.com>
2022-11-29 10:47:00 +0000

    Fix double gender select bug
    The products slice was reset when the user selected the same gender

--- client/src/components/HeaderGenderSelect/HeaderGenderSelect.tsx
+++ client/src/components/HeaderGenderSelect/HeaderGenderSelect.tsx
@@ -6,13 +6,17 @@ import { Gender } from '../../http/categoryApi/categoryApi.types'
 import { selectGender } from '../../store/reducers/productParamsSlice/productParamsSliceActions'
 import { selectProductGender } from '../../store/reducers/productParamsSlice/productParamsSliceSelectors'
 import { useAppDispatch, useAppSelector } from '../../hooks/redux'
+import { useNavigate } from 'react-router-dom'
+import { Routes } from '../../utils/consts'
 
 const HeaderGenderSelect = ({ className, ...props }: IFullSizeImage) => {
   const selectedGender = useAppSelector(selectProductGender)
   const dispatch = useAppDispatch()
+  const navigate = useNavigate()
 
   const handleGenderSelect = (value: Gender) => {
-    dispatch(selectGender(value))
+    if (value !== selectedGender) dispatch(selectGender(value))
+    navigate(Routes.SHOP_ROUTE)
   }
 
   return (
